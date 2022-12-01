83bc24137e71702fc0f9eb478bb80657ce5fa34b
Nishith <lnu.nish@northeastern.edu>
2022-12-01 17:25:46 -0500

    Added models for Shelter and Population directory

---
+++ PRS/AnimalRescueSystem/src/business/population/UserAccount.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.population;
+
+/**
+ *
+ * @author nishi
+ */
+public class UserAccount {
+    
+    String email;
+    String password;
+    String role;
+    String userAccountId;
+    
+}
