48cc7ec7ffb3a88223083e5e129158c194de6e1d
Nishith <lnu.nish@northeastern.edu>
2022-12-04 12:47:02 -0500

    Added models and UI for government enterprise

---
+++ PRS/AnimalRescueSystem/src/business/Government/GovernmentManager.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.Government;
+
+/**
+ *
+ * @author nishi
+ */
+public class GovernmentManager {
+    String governmentManagerId;
+    int yearsOfExperience;
+    int phoneNumber;
+    float salary;
+    
+}
