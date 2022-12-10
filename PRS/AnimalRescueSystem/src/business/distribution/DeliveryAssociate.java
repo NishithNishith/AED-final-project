36f208dcc80ed314d64235be8d1345dfff1cdd13
Nishith <lnu.nish@northeastern.edu>
2022-12-10 00:36:08 -0500

    Hospital merge fix

--- PRS/AnimalRescueSystem/src/business/distribution/DeliveryAssociate.java
+++ PRS/AnimalRescueSystem/src/business/distribution/DeliveryAssociate.java
@@ -14,21 +14,7 @@ public class DeliveryAssociate extends Person{
     public int deliveryAssociateID;
     public String phoneNumber;
     public String workStatus;
-<<<<<<< HEAD
-    public String nameOfDA;
-    
-    public DeliveryAssociate(String personID, String name, int age, String gender, String dateOfBirth, int deliveryAssociateID, String phoneNumber, String workStatus, String nameOfDA){
-        
-      //  super(personID, name, age, gender, dateOfBirth);
-        this.deliveryAssociateID = deliveryAssociateID;
-        this.phoneNumber = phoneNumber;
-        this.workStatus = workStatus;
-        this.nameOfDA = nameOfDA;
-               
-    }
-=======
     public String nameOfDA;    
->>>>>>> c4667302192db8820613e994706bbe16288a777a
 
     public int getDeliveryAssociateID() {
         return deliveryAssociateID;
